d0f15a2efdcc0bc91e1435ca07ae8910698f1ea9
Bruno Rondon da Silva <bruno.engmeca@gmail.com>
2020-04-26 15:02:13 -0400

    Ajustes e teste da api com postman com os verbos get, post, put e delete

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,3 +1,12 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 module.exports = mongoose.connect('mongodb://localhost/mymoney')
+
+mongoose.Error.messages.general.required = "O atributo '{PATH}' e obrigatorio."
+mongoose.Error.messages.Number.min =
+    "O '{VALUE}' informado e menor que o limite minimo de '{MIN}'."
+
+mongoose.Error.messages.Number.max =
+    "O '{VALUE}' infomado e maior que o limite maximo de '{MAX}'."
+mongoose.Error.messages.String.enum =
+    "'{VALUE}' nao e valido para o atributo '{PATH}'."
